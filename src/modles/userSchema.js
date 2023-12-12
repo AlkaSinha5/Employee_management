b0cd0236a8dd1dd48bccda7d7d6451077b4ed7c3
Alka Sinha <sinhaalka802211@gmail.com>
2023-12-12 10:03:50 +0530

    day1

---
+++ src/modles/userSchema.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+//   roleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Role' },
+  FirstName: String,
+  LastName: String,
+  Email: String,
+  Password: String,
+  
+});
+
+export default mongoose.model('User', userSchema);
---
+++ src/routes/userRoute.js
@@ -0,0 +1,31 @@
+import  express  from "express";
+import { DeleteUser, GetUserById, UpdateUser, GetUsers, AddUser, LoginUser } from "../controllers/user.controllers.js";
+// import { userSchema } from '../validators/user.validators.js'
+// import { verifyToken } from "../helper/token_verify.js";
+// import { isSuperAdmin } from "../middleware/isSuperAdmin.js";
+
+
+
+
+const router =express.Router();
+// const validateUser = (req, res, next) => {
+//     const { error } = userSchema.validate(req.body);
+  
+//     if (error) {
+//       return res.status(400).json({ error: error.details[0].message });
+//     }
+  
+//     next();
+//   };
+  
+router.post("/addUser", AddUser);
+router.post('/login', LoginUser);
+router.get("/getUsers", GetUsers);
+router.delete("/deleteUser/:id", DeleteUser);
+router.put("/updateUser/:id",UpdateUser );
+router.get("/getUserById/:id", GetUserById);
+
+
+
+
+export default router ;
