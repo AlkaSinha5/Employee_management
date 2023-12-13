8647d4338a94f45dc26d3caeab61e1d73354d444
Alka Sinha <sinhaalka802211@gmail.com>
2023-12-13 12:38:46 +0530

    done

--- src/routes/employeeRoute.js
+++ src/routes/employeeRoute.js
@@ -1,10 +1,14 @@
 import  express  from "express";
-import { AddEmployee } from "../controllers/employee.controllers.js";
+import { AddEmployee, DeleteEmployee, GetEmployee, GetEmployeeById, UpdateEmployee } from "../controllers/employee.controllers.js";
 
 
 const router =express.Router();
 
 router.post("/addEmployee", AddEmployee);
+router.get("/getEmployee" ,GetEmployee);
+router.delete("/deteteEmployee/:id", DeleteEmployee);
+router.put("/updateEmployee/:id", UpdateEmployee);
+router.get("/getEmployeeById/:id",  GetEmployeeById);
 
 
 
--- src/services/employee.services.js
+++ src/services/employee.services.js
@@ -10,7 +10,8 @@ export const addEmployee = asyncHandler(async (req, res) => {
         LastName,
         Email,
         PhoneNumber,
-        ProfilePicture, } = req.body;
+        ProfilePicture,
+        jobType,  } = req.body;
     
 const employee = await Employee.create({
     UserID,
@@ -19,6 +20,7 @@ const employee = await Employee.create({
     Email,
     PhoneNumber,
     ProfilePicture,
+    jobType, 
         });
   
         res.status(201).json({
@@ -33,4 +35,61 @@ const employee = await Employee.create({
     }
   });
 
+  export const getEmployee = asyncHandler(async (paginationOptions,filter,sort) => {
+    try {
+      const { page, size } = paginationOptions;
+      const totalDocuments = await Employee.countDocuments(filter);
+      const totalPages = Math.ceil(totalDocuments / size);
+      const skip = (page - 1) * size;
   
+      const collation = {
+        locale: 'en',  
+        strength: 2,
+      };
+  
+      const success = await Employee.find(filter)
+      .collation(collation)
+      .sort(sort)
+      .skip(skip)
+      .limit(size);
+  
+      return {
+        page,
+        size,
+        data: success,
+        previousPage: page > 1 ? page - 1 : null,
+        nextPage: page < totalPages ? page + 1 : null,
+        totalDocuments,
+      };
+    } catch (e) {
+      console.log(e);
+      throw new Error(e);
+    }
+  }); 
+
+  export const deleteEmployee = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const success = await Employee.findByIdAndDelete(id);
+    if (success) {
+      res.status(200).send({ success, message: "Ok deleted ......" });
+    } else {
+    
+      res.status(404).send({massage: "ID not found "})
+        return { error: "not deleted..." };
+    }
+  });
+  
+export const getEmployeeById = asyncHandler(async (id) => {
+    const success = await Employee.findById(id);
+    console.log(success);
+    return success;
+  });
+  
+export const updateEmployee = asyncHandler(async (id, updatedData) => {
+    const success = await Employee.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    console.log(success);
+    return success;
+  });
+
