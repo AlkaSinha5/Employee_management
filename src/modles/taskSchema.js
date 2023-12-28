1303e0404c1fa62374e0ae71dbedf7110b8ef955
Alka Sinha <sinhaalka802211@gmail.com>
2023-12-28 13:32:48 +0530

    user

--- src/modles/taskSchema.js
+++ src/modles/taskSchema.js
@@ -1,21 +1,18 @@
 import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
-    UserID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  task: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  Date:{
-    type:Date,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  UserID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  tasks: [
+    {
+      task: String,
+      completed: Boolean,
+      Date: Date,
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
 
 export default mongoose.model('Task', taskSchema);
--- src/services/task.service.js
+++ src/services/task.service.js
@@ -4,44 +4,42 @@ import User from "../modles/userSchema.js"
 
 // Create a task
 export const addTaskData = asyncHandler(async (req, res) => {
-    try {
-      const { UserID, task, completed } = req.body;
-      
-      const user = await User.findOne({ _id: UserID });
-      
-      if (!user) {
-        return res.status(404).json({
-          success: false,
-          error: 'User not found',
-        });
-      }
-  
-      const newTask = await Task.create({
-        UserID,
-        task,
-        completed,
-      });
-  
-      user.tasks.push(newTask._id); 
-      await user.save();
-  
-      res.status(201).json({
-        success: true,
-        data: newTask,
-      });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(400).json({
+  try {
+    const { UserID, tasks } = req.body;
+
+    const user = await User.findOne({ _id: UserID });
+
+    if (!user) {
+      return res.status(404).json({
         success: false,
-        error: error.message,
+        error: 'User not found',
       });
     }
-  });
+
+    // Assuming tasks is an array of task objects
+    const newTasks = await Task.insertMany(tasks.map(task => ({ ...task, UserID })));
+
+    // Assuming each task has an _id property
+    user.tasks.push(...newTasks.map(task => task._id));
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      data: newTasks,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 // Get all tasks
 export const getAllTasks = asyncHandler(async (req, res) => {
   try {
-    const tasks = await Task.find();
+    const tasks = await Task.find().lean();
 
     res.status(200).json({
       success: true,
@@ -59,7 +57,7 @@ export const getAllTasks = asyncHandler(async (req, res) => {
 export const getTaskById = asyncHandler(async (req, res) => {
   try {
     const id = req.params.id;
-    const task = await Task.findById(id);
+    const task = await Task.findById(id).lean();
 
     if (!task) {
       return res.status(404).json({
@@ -84,7 +82,7 @@ export const getTaskById = asyncHandler(async (req, res) => {
 export const deleteTask = asyncHandler(async (req, res) => {
   try {
     const id = req.params.id;
-    const task = await Task.findByIdAndDelete(id);
+    const task = await Task.findByIdAndDelete(id).lean();
 
     if (!task) {
       return res.status(404).json({
@@ -104,32 +102,31 @@ export const deleteTask = asyncHandler(async (req, res) => {
 
 // Update task by ID
 export const updateTask = asyncHandler(async (req, res) => {
-    try {
-      const id = req.params.id;
-      const { title, description, completed } = req.body;
-  
-      const task = await Task.findByIdAndUpdate(
-        id,
-        { title, description, completed },
-        { new: true, runValidators: true }
-      );
-  
-      if (!task) {
-        return res.status(404).json({
-          success: false,
-          error: 'Task not found',
-        });
-      }
-  
-      res.status(200).json({
-        success: true,
-        data: task,
-      });
-    } catch (error) {
-      res.status(500).json({
+  try {
+    const id = req.params.id;
+    const { title, description, completed } = req.body;
+
+    const task = await Task.findByIdAndUpdate(
+      id,
+      { title, description, completed },
+      { new: true, runValidators: true }
+    ).lean();
+
+    if (!task) {
+      return res.status(404).json({
         success: false,
-        error: 'Server Error',
+        error: 'Task not found',
       });
     }
-  });
-  
+
+    res.status(200).json({
+      success: true,
+      data: task,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
+});
